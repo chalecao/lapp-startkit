5ab49167908131230730650ccf15706441ca79b9
haomou.chh <haomou.chh@alibaba-inc.com>
2018-05-25 11:12:55 +0800

    add uglify

--- tools/build.js
+++ tools/build.js
@@ -11,6 +11,9 @@ const del = require('del');
 const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
 const postcss = require('rollup-plugin-postcss');
+const uglify = require('rollup-plugin-uglify').uglify;
+const minify = require('uglify-es').minify;
+const buble = require('rollup-plugin-buble');
 // PostCSS plugins
 const simplevars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
@@ -28,30 +31,34 @@ promise = promise.then(() => del(['dist/*']));
     promise = promise.then(() => rollup.rollup({
         input: 'src/index.js',
         external: Object.keys(pkg.dependencies),
-        plugins: [postcss({
-            plugins: [
-                simplevars(),
-                nested(),
-                cssnext({ warnForDuplicates: false, }),
-                cssnano()],
-            extensions: ['.css']
-        }),
-        babel(Object.assign({
-            babelrc: false,
-            exclude: 'node_modules/**',
-            runtimeHelpers: true,
-            presets: [[
-                'env',
-                {
-                    'modules': false
-                }
-            ]],
-            plugins: [
-                ['transform-react-jsx', {
-                    'pragma': 'l'
-                }], "external-helpers"
-            ]
-        }))]
+        plugins: [
+            postcss({
+                plugins: [
+                    simplevars(),
+                    nested(),
+                    cssnext({ warnForDuplicates: false, }),
+                    cssnano()],
+                extensions: ['.css']
+            }),
+            babel(Object.assign({
+                babelrc: false,
+                exclude: 'node_modules/**',
+                runtimeHelpers: true,
+                presets: [[
+                    'env',
+                    {
+                        'modules': false
+                    }
+                ]],
+                plugins: [
+                    ['transform-react-jsx', {
+                        'pragma': 'l'
+                    }], "external-helpers"
+                ]
+            })),
+            buble(),
+            uglify({ mangle: { toplevel: true } }, minify)
+        ]
     }).then(bundle => bundle.write({
         file: `dist/${format === 'cjs' ? 'index' : `index.${format}`}.js`,
         format,
